d587ba98f82e2341c6728f6932f13382d185e7ad
Josh McMillan <josh@joshmcmillan.com>
2025-11-18 08:18:18 +0000

    Cache Spektrix responses for 1 minute

--- app/services/spektrix_service.rb
+++ app/services/spektrix_service.rb
@@ -2,11 +2,15 @@ class SpektrixService
   include Singleton
 
   def get_instances(client_name:, event_id:)
-    get("https://system.spektrix.com/#{client_name}/api/v3/events/#{event_id}/instances")
+    Rails.cache.fetch("#{client_name}/#{event_id}", expires_in: 1.minute) do
+      get("https://system.spektrix.com/#{client_name}/api/v3/events/#{event_id}/instances")
+    end
   end
 
   def get_instance_status(client_name:, instance_id:)
-    get("https://system.spektrix.com/#{client_name}/api/v3/instances/#{instance_id}/status")
+    Rails.cache.fetch("#{client_name}/#{instance_id}", expires_in: 1.minute) do
+      get("https://system.spektrix.com/#{client_name}/api/v3/instances/#{instance_id}/status")
+    end
   end
 
   private def get(...)
